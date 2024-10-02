ed54f568da36745f9f94ad1721e037bc48a647d9
Jovica Kuzmanovic <jovica04@outlook.com>
2024-10-01 17:00:07 -0700

    3 new addons, random, txt and register file

--- register.js
+++ register.js
@@ -1,108 +1,96 @@
-// const { error } = require('console')
-//Importing Node modules
-
-function register(username, password) {
-    return new Promise((resolve, reject) => {
-      fs.readFile('database.txt', 'utf8', (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          const users = data.split('\n');
-          const existingUser = users.find(user => user.split(',')[0] === username);
-  
-          if (existingUser) {
-            reject(new Error('User already exists'));
-          } else {
-            const newUser = `${username},${password}\n`;
-            fs.appendFile('database.txt', newUser, err => {
-              if (err) {
-                reject(err);
-              } else {
-                resolve('Registration successful');
-              }
-            });
-          }
-        }
-      });
-    });
-  }
-
-  const fs = require('fs');
-
-function createBlog(blogName) {
-  return new Promise((resolve, reject) => {
-    fs.mkdir(blogName, (err) => {
-      if (err) {
-        if (err.code === 'EEXIST') {
-          reject(new Error('Blog with that name already exists'));
-        } else {
-          reject(err);
-        }
-      } else {
-        resolve('Blog created successfully');
-      }
-    });
-  });
-}
-
-function createPost(postTitle, postContent, blogName) {
-  return new Promise((resolve, reject) => {
-    fs.access(blogName, (err) => {
-      if (err) {
-        reject(new Error('Blog does not exist'));
-      } else {
-        const fileName = postTitle.replace(/\s/g, '_');
-        fs.writeFile(`${blogName}/${fileName}.txt`, postContent, (err) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve('Post created successfully');
-          }
-        });
-      }
-    });
-  });
-}
+const fs = require('fs')
 
 const fs = require('fs');
+const promisify = require('util').promisify;
 
-function likePost(blogName, postTitle, username) {
-  return new Promise((resolve, reject) => {
-    // Check if the user exists in the database
-    fs.readFile('database.txt', 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
+const readFileB = promisify(fs.readFile);
+const appendFileB = promisify(fs.appendFile);
+
+function new_register(username, password) {
+  return readFileB('database.txt', 'utf8')
+    .then(data => {
+      const users = data.split('\n');
+      const currentUser = users.find(user => user.split(',')[0] === username);
+
+      if (currentUser) {
+        throw new Error('User already exists');
       } else {
-        const users = data.split('\n');
-        const existingUser = users.find(user => user.split(',')[0] === username);
+        const newRegisteredUser = `${username},${password}\n`;
+        return appendFileB('database.txt', newRegisteredUser);
+      }
+    })
+    .then(() => {
+      return 'Your registration is completed';
+    })
+    .catch(err => {
+      throw err;
+    });
+}
+// This function will create a new directory, if that directorey already exists it will send an error message to the user
+const mkdirN = promisify(fs.mkdir);
 
-        if (!existingUser) {
-          reject(new Error('User does not exist'));
-        } else {
-          // Find the post and update the likes counter and likedBy line
-          const fileName = postTitle.replace(/\s/g, '_');
-          fs.readFile(`${blogName}/${fileName}.txt`, 'utf8', (err, postContent) => {
-            if (err) {
-              reject(err);
-            } else {
-              const lines = postContent.split('\n');
-              const likesLine = lines[0];
-              const likes = parseInt(likesLine.split(' ')[1]) + 1;
-              lines[0] = `Likes: ${likes}`;
-              const likedByLine = lines[1];
-              lines[1] = likedByLine.replace('you', `you,${username}`);
-              const updatedPostContent = lines.join('\n');
-              fs.writeFile(`${blogName}/${fileName}.txt`, updatedPostContent, (err) => {
-                if (err) {
-                  reject(err);
-                } else {
-                  resolve('Post liked successfully');
-                }
-              });
-            }
-          });
-        }
+function createBlog(blogName) {
+  return mkdirN(blogName)
+    .then(() => {
+      return 'Your Blog has been created';
+    })
+    .catch(err => {
+      if (err.code === 'EEXIST') {
+        throw new Error('That Blog already exists');
+      } else {
+        throw err;
       }
     });
-  });
+}
+
+const accessN = promisify(fs.access);
+const writeFileN = promisify(fs.writeFile);
+
+function createPost(postTitle, postContent, blogName) {
+  return accessN(blogName)
+    .then(() => {
+      const fileName = postTitle.replace(/\s/g, '_');
+      return writeFileN(`${blogName}/${fileName}.txt`, postContent);
+    })
+    .then(() => {
+      return 'Post created successfully';
+    })
+    .catch(err => {
+      throw err;
+    });
+}
+
+const readFileO = promisify(fs.readFile);
+const writeFileO = promisify(fs.writeFile);   
+
+
+function likePost(blogName, postTitle, username) {
+  return readFileO('database.txt', 'utf8')
+    .then(data => {
+      const users = data.split('\n');
+      const existingUser = users.find(user => user.split(',')[0] === username);
+
+      if (!existingUser) {
+        throw new Error('User does not exist');
+      } else {
+        const fileName = postTitle.replace(/\s/g, '_');
+        return readFileO(`${blogName}/${fileName}.txt`, 'utf8');
+      }
+    })
+    .then(postContent => {
+      const lines = postContent.split('\n');
+      const likesLine = lines[0];
+      const likes = parseInt(likesLine.split(' ')[1]) + 1;
+      lines[0] = `Likes: ${likes}`;
+      const likedByLine = lines[1];
+      lines[1] = likedByLine.replace('you', `you,${username}`);
+      const updatedPostContent = lines.join('\n');
+      return writeFileO(`${blogName}/${fileName}.txt`, updatedPostContent);
+    })
+    .then(() => {
+      return 'Post liked successfully';
+    })
+    .catch(err => {
+      throw err;
+    });
 }
